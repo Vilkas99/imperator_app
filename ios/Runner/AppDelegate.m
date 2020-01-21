862646d11bee64bdccab2e8ce8659c00bd595c50
Víctor Mancera <victorium99@hotmail.com>
2020-01-20 20:08:44 -0600

    Archivos iniciales
    -Vinculación con api de Maps
    -Tarjetas de restaurantes

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,16 @@
+#import "AppDelegate.h"
+#import "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+      [GMSServices provideAPIKey:@"AIzaSyD1XxOEGSKW4Qee2H3hZ4HAoxcdxqdDVEE"];
+
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
